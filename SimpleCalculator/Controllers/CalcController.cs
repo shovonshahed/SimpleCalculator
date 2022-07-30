d3d71904fc475527b76cfe5a756600565e1e2730
Shahed Islam <shahedislamshovon@gmail.com>
2022-07-31 00:13:04 +0600

    Complete project of Simple Calculator

---
+++ SimpleCalculator/Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCalculator.Models;
+
+namespace SimpleCalculator.Controllers;
+
+public class CalcController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View(new Calc());
+    }
+    
+    [HttpPost]
+    public IActionResult Index(Calc c, string calculate)
+    {
+        if (calculate == "add")
+        {
+            c.Tot = c.No1 + c.No2;
+        }
+        else if (calculate == "sub")
+        {
+            c.Tot = c.No1 - c.No2;
+        }
+        else if (calculate == "multi")
+        {
+            c.Tot = c.No1 * c.No2;
+        }
+        else if (calculate == "div")
+        {
+            c.Tot = c.No1 / c.No2;
+        }
+
+        return View(c);
+    }
+    
+}
---
+++ SimpleCalculator/Models/Calc.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleCalculator.Models;
+
+public class Calc
+{
+    public double No1 { get; set; }
+    public double No2 { get; set; }
+    public double Tot { get; set; }
+}
